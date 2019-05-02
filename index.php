7d0c9652afb005f1753743236633e7e0921d77ab
Navraj Khanal <34544499+teamanveshak@users.noreply.github.com>
2019-05-02 12:56:17 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+ <link rel="stylesheet" type="text/css" href="css/style.css">
+<title>
+</title>
+</head>
+<body>
+	<div id="container" style="text-align:center;">
+<div id='navtop'>
+<center><a href="#">php</a></center>
+</div>
+<div id='content'>
+	<h3>What is php?</h3>
+	<p>PHP is open source server server side scripting language</p>
+	<p>PHP is acronym for:"Hypertext Preprocessor"</p>
+	<p style="color: red; font-weight:bold;">PHP is powerful programming language.</p></div>
+	<br>
+	<div id="content">
+		<h3>Definition</h3>
+		<p>PHP is server programming language, in which the codes are executed  on the server and the result is sent back to browser as plain HTML text</p></div>
+
+<br>
+
+		<div id="content">
+			<h3>What is PHP file?</h3>
+			<p>PHP file has .php extension</p>
+			<p>PHP file can contain plain text,html,css,javascript and PHP code</p>
+</div>
+<br>
+<div id="content">
+	<h3>What php can do?</h3>
+	<p>PHP is sever programming language</p>
+	<p>PHP can be used to create ,read,write,open and delete files on servers</p>
+	<p>PHP can be used to read,write,modify and delete data in database</p>
+	<p>PHP can be used to send and recieve the cookies</p>
+	<p>PHP can be used to encrypt the data</p>
+	<p>PHP can be used to collect the form data</p>
+	<p>PHP can be used to control the user access</p>
+</div>
+<br>
+	<div id="content"></div>
+	<h3>Advaantages</h3>
+	<p>PHP is free and open source</p>
+	<p>Support all database sytsem</p>
+	<p>Runs on all available server:apache IIS</p>
+	<p>It is easy to learn</p>
+	<br>
+</div>
+</body>
+
+</html>
